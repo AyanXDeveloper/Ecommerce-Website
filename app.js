ebe4c7932fe756a84addcfd1086cecbec7d3ebe7
AyanXDeveloper <ayyanzaveri@gmail.com>
2025-11-01 12:12:31 +0500

    All Products Dynamically Added Through Javascript

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ let fragrance = {
                 price: "1.490",
             },
             {
-                imgURL: "./assets/Untitled design (25).pn",
+                imgURL: "./assets/Untitled design (25).png",
                 name: "Hisar",
                 description: "Smoky Vanilla Pleasure",
                 rating: 18,
@@ -117,6 +117,129 @@ let fragrance = {
                 description: "Perfumes that fit anywhere",
                 rating: 87,
                 price: "4,290",
+            },
+        ]
+
+    },
+    summerDeals: {
+        title: "Summer Deals",
+        items: [
+            {
+                imgURL: "./assets/Untitled design (27).png",
+                name: "Floral Body Mist",
+                description: "Women’s Favorite Floral Mist",
+                rating: 21,
+                price: "680",
+            },
+            {
+                imgURL: "./assets/perfume-17.webp",
+                name: "Creamy Pineapple",
+                description: "2nd Best Selling Perfume",
+                rating: 12,
+                price: "2,600",
+            },
+            {
+                imgURL: "./assets/perfume19.webp",
+                name: "Israr",
+                description: "Timeless Woody Scent",
+                rating: 9,
+                price: "1.520",
+            },
+            {
+                imgURL: "./assets/Untitled design (28).png",
+                name: "Lost Body Mist",
+                description: "Most Luxurious Flowery Aroma",
+                rating: 14,
+                price: "770",
+            },
+            {
+                imgURL: "./assets/SandCity1x1.webp",
+                name: "Sand City",
+                description: "Legendary Men's Perfume",
+                rating: 29,
+                price: "1,270",
+            },
+        ]
+
+    },
+    oud: {
+        title: "Oud",
+        items: [
+            {
+                imgURL: "./assets/oud-1.webp",
+                name: "Inferno",
+                description: "Our Best Oud Fragrance",
+                rating: 83,
+                price: "1,390",
+            },
+            {
+                imgURL: "./assets/oud-2.webp",
+                name: "Amber Oud",
+                description: "Most Popular Rose Oud",
+                rating: 7,
+                price: "2,750",
+            },
+            {
+                imgURL: "./assets/oud-3.webp",
+                name: "Oud E Maazi",
+                description: "Classic Oud Rose Combo",
+                rating: 19,
+                price: "2.190",
+            },
+            {
+                imgURL: "./assets/oud-4.webp",
+                name: "Addiction",
+                description: "Best Selling Oriental Perfume",
+                rating: 18,
+                price: "3.190",
+            },
+            {
+                imgURL: "./assets/oud-5.webp",
+                name: "Zargiya",
+                description: "The Elite Scent!",
+                rating: 11,
+                price: "2,480",
+            },
+        ]
+
+    },
+    ahl_E_Oud: {
+        title: "Ahl E Oud",
+        items: [
+            {
+                imgURL: "./assets/Untitled design (29).png",
+                name: "Catch 22 - Roller",
+                description: "Our #1 Best Seller 🏆",
+                rating: 9,
+                price: "750",
+            },
+            {
+                imgURL: "./assets/Untitled design (30).png",
+                name: "Merry Me - Roller",
+                description: "#1 Women's Best-Seller 🏆",
+                rating: 1,
+                price: "850",
+            },
+            {
+                imgURL: "./assets/Untitled design (31).png",
+                name: "Smash My Head - Roller",
+                description: "Our Top Rated Fragrance ⭐",
+                rating: 2,
+                price: "1.090",
+            },
+            {
+                imgURL: "./assets/Untitled design (32).png",
+                name: "Amber Oud - Roller",
+                description: "2nd Most Popular Rose & Oud Combo",
+                rating: 2,
+                price: "990",
+            },
+            {
+                imgURL: "./assets/Untitled design (33).png",
+                name: "Tipping Point - Roller",
+                description: "Our #1 Office Wear! 👔",
+                rating: 5,
+                price: "980",
             },
         ]
 
@@ -152,7 +275,7 @@ let fragrance = {
     for (let i = 0; i < fragrance.sensoryRange.items.length; i++) {
         cards2.innerHTML += `
                 <div class="card" style="width: 18rem;">
-                    <img width=280 height=300 src="${fragrance.sensoryRange.items[i].imgURL}" alt="...">
+                    <img src="${fragrance.sensoryRange.items[i].imgURL}" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${fragrance.sensoryRange.items[i].name}</h5>
                         <p class="card-text-seller">${fragrance.sensoryRange.items[i].description}</p>
@@ -176,7 +299,7 @@ let fragrance = {
     for (let i = 0; i < fragrance.poeticRange.items.length; i++) {
         cards3.innerHTML += `
                 <div class="card" style="width: 18rem;">
-                    <img width=280 height=300 src="${fragrance.poeticRange.items[i].imgURL}" alt="...">
+                    <img src="${fragrance.poeticRange.items[i].imgURL}" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${fragrance.poeticRange.items[i].name}</h5>
                         <p class="card-text-seller">${fragrance.poeticRange.items[i].description}</p>
@@ -191,6 +314,54 @@ let fragrance = {
                         <div class="perfume-amount">
                             <p class="card-text-from">from</p>
                             <p class="per-price">Rs.${fragrance.poeticRange.items[i].price}</p>
+                        </div>
+                    </div>
+                </div>`
+    }
+
+    let cards4 = document.getElementById("cards4")
+    for (let i = 0; i < fragrance.summerDeals.items.length; i++) {
+        cards4.innerHTML += `
+                <div class="card" style="width: 18rem;">
+                    <img src="${fragrance.summerDeals.items[i].imgURL}" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${fragrance.summerDeals.items[i].name}</h5>
+                        <p class="card-text-seller">${fragrance.summerDeals.items[i].description}</p>
+                        <div class="rating-cards">
+                            <i class="fa-solid fa-star fa-sm" style="color: #d57760;"></i>
+                            <i class="fa-solid fa-star fa-sm" style="color: #d57760;"></i>
+                            <i class="fa-solid fa-star fa-sm" style="color: #d57760;"></i>
+                            <i class="fa-solid fa-star fa-ms" style="color: #d57760;"></i>
+                            <i class="fa-solid fa-star fa-sm" style="color: #d57760;"></i>
+                            <p class="rating-amount">( ${fragrance.summerDeals.items[i].rating} )</p>
+                        </div>
+                        <div class="perfume-amount">
+                            <p class="card-text-from">from</p>
+                            <p class="per-price">Rs.${fragrance.summerDeals.items[i].price}</p>
+                        </div>
+                    </div>
+                </div>`
+    }
+
+    let cards5 = document.getElementById("cards5")
+    for (let i = 0; i < fragrance.oud.items.length; i++) {
+        cards5.innerHTML += `
+                <div class="card" style="width: 18rem;">
+                    <img src="${fragrance.oud.items[i].imgURL}" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${fragrance.oud.items[i].name}</h5>
+                        <p class="card-text-seller">${fragrance.oud.items[i].description}</p>
+                        <div class="rating-cards">
+                            <i class="fa-solid fa-star fa-sm" style="color: #d57760;"></i>
+                            <i class="fa-solid fa-star fa-sm" style="color: #d57760;"></i>
+                            <i class="fa-solid fa-star fa-sm" style="color: #d57760;"></i>
+                            <i class="fa-solid fa-star fa-ms" style="color: #d57760;"></i>
+                            <i class="fa-solid fa-star fa-sm" style="color: #d57760;"></i>
+                            <p class="rating-amount">( ${fragrance.oud.items[i].rating} )</p>
+                        </div>
+                        <div class="perfume-amount">
+                            <p class="card-text-from">from</p>
+                            <p class="per-price">Rs.${fragrance.oud.items[i].price}</p>
                         </div>
                     </div>
                 </div>`
